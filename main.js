d800d03a507d847bb8fde7b46b8687b76bae1319
Rahul <rahulreddie30@gmail.com>
2021-12-23 21:32:03 +0530

    committed

---
+++ main.js
@@ -0,0 +1,201 @@
+console.log("click on add to cart images once and twice to view product photos");
+
+document.getElementById("img1").addEventListener("focus", myFunction());
+function myFunction(){
+    setNewImage();
+    setNewImagee();
+    setOldImage();
+}
+function setNewImage(){
+    document.getElementById("img1").src="images/gallery-2.jpg";
+}
+function setOldImage(){
+    document.getElementById("img1").src="images/gallery-1.jpg";
+}
+function setNewImagee(){
+    document.getElementById("img1").src="images/gallery-3.jpg";
+}
+
+
+document.getElementById("img2").addEventListener("focus", myFunction2());
+
+function myFunction2(){
+    setNewImage2();
+    setNewImagee2();
+    setOldImage2();
+}
+function setNewImage2(){
+    document.getElementById("img2").src="images/gallery2-2.webp";
+}
+function setOldImage2(){
+    document.getElementById("img2").src="images/product-2.jpg";
+}
+function setNewImagee2(){
+    document.getElementById("img2").src="images/gallery2-4.webp";
+}
+document.getElementById("img3").addEventListener("focus", myFunction3());
+
+function myFunction3(){
+    setNewImage3();
+    setNewImagee3();
+    setOldImage3();
+}
+function setNewImage3(){
+    document.getElementById("img3").src="images/gallery3-2.jpg";
+}
+function setOldImage3(){
+    document.getElementById("img3").src="images/gallery3-1.jpg";
+}
+function setNewImagee3(){
+    document.getElementById("img3").src="images/gallery3-4.jpg";
+}
+
+document.getElementById("img4").addEventListener("focus", myFunction4());
+
+function myFunction4(){
+    setNewImage4();
+    setNewImagee4();
+    setOldImage4();
+}
+function setNewImage4(){
+    document.getElementById("img4").src="images/gallery4-1.jpg";
+}
+function setOldImage4(){
+    document.getElementById("img4").src="images/product44.webp";
+}
+function setNewImagee4(){
+    document.getElementById("img4").src="images/gallery4-3.jpg";
+}
+document.getElementById("img5").addEventListener("focus", myFunction5());
+
+function myFunction5(){
+    setNewImage5();
+    setNewImagee5();
+    setOldImage5();
+}
+function setNewImage5(){
+    document.getElementById("img5").src="images/gallery5-2.jpg";
+}
+function setOldImage5(){
+    document.getElementById("img5").src="images/product-5.jpg";
+}
+function setNewImagee5(){
+    document.getElementById("img5").src="images/gallery5-3.jpg";
+}
+document.getElementById("img6").addEventListener("focus", myFunction6());
+
+function myFunction6(){
+    setNewImage6();
+    setNewImagee6();
+    setOldImage6();
+}
+function setNewImage6(){
+    document.getElementById("img6").src="images/gallery6-1.jpg";
+}
+function setOldImage6(){
+    document.getElementById("img6").src="images/product-6.jpg";
+}
+function setNewImagee6(){
+    document.getElementById("img6").src="images/gallery6-2.jpg";
+}
+
+document.getElementById("img7").addEventListener("focus", myFunction7());
+function myFunction7(){
+    setNewImage7();
+    setNewImagee7();
+    setOldImage7();
+}
+function setNewImage7(){
+    document.getElementById("img7").src="images/gallery7-3.jpg";
+}
+function setOldImage7(){
+    document.getElementById("img7").src="images/product-7.jpg";
+}
+function setNewImagee7(){
+    document.getElementById("img7").src="images/gallery7-2.jpg";
+}
+
+document.getElementById("img8").addEventListener("focus", myFunction8());
+function myFunction8(){
+    setNewImage8();
+    setNewImagee8();
+    setOldImage8();
+}
+function setNewImage8(){
+    document.getElementById("img8").src="images/gallery8-2.jpg";
+}
+function setOldImage8(){
+    document.getElementById("img8").src="images/product-8.jpg";
+}
+function setNewImagee8(){
+    document.getElementById("img8").src="images/gallery8-4.jpg";
+}
+
+document.getElementById("img9").addEventListener("focus", myFunction9());
+function myFunction9(){
+    setNewImage9();
+    setNewImagee9();
+    setOldImage9();
+}
+function setNewImage9(){
+    document.getElementById("img9").src="images/gallery9-2.jpg";
+}
+function setOldImage9(){
+    document.getElementById("img9").src="images/product-9.webp";
+}
+function setNewImagee9(){
+    document.getElementById("img9").src="images/gallery9-3.webp";
+}
+
+document.getElementById("img10").addEventListener("focus", myFunction10());
+function myFunction10(){
+    setNewImage10();
+    setNewImagee10();
+    setOldImage10();
+}
+function setNewImage10(){
+    document.getElementById("img10").src="images/gallery10-2.jpg";
+}
+function setOldImage10(){
+    document.getElementById("img10").src="images/product-10.jpg";
+}
+function setNewImagee10(){
+    document.getElementById("img10").src="images/gallery10-3.jpg";
+}
+
+document.getElementById("img11").addEventListener("focus", myFunction11());
+function myFunction11(){
+    setNewImage11();
+    setNewImagee11();
+    setOldImage11();
+}
+function setNewImage11(){
+    document.getElementById("img11").src="images/gallery11-2.jpg";
+}
+function setOldImage11(){
+    document.getElementById("img11").src="images/product-11.jpg";
+}
+function setNewImagee11(){
+    document.getElementById("img11").src="images/gallery11-3.jpg";
+}
+
+document.getElementById("img12").addEventListener("focus", myFunction12());
+function myFunction12(){
+    setNewImage12();
+    setNewImagee12();
+    setOldImage12();
+}
+function setNewImage12(){
+    document.getElementById("img12").src="images/gallery12-2.webp";
+}
+function setOldImage12(){
+    document.getElementById("img12").src="images/product-12.webp";
+}
+function setNewImagee12(){
+    document.getElementById("img12").src="images/gallery12-3.webp";
+}
+
+
+
+
+
